529b20e18cf84f7f5fddca144750ce904fc381f9
subahan ali <subahanislam523@gmail.com>
2024-09-11 02:26:03 +0600

    baner slider page

--- src/Pages/Home/BannerSlider/BannerSlider.jsx
+++ src/Pages/Home/BannerSlider/BannerSlider.jsx
@@ -9,6 +9,8 @@ import "swiper/css/pagination";
 import { Autoplay, Pagination } from "swiper/modules";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
+import image from "../../../assets/carousel4.jpg"
+import image1 from "../../../assets/carousel5.jpg"
 const BannerSlider = () => {
   return (
     <Swiper
@@ -24,14 +26,14 @@ const BannerSlider = () => {
         <div
           className="hero min-h-screen"
           style={{
-            backgroundImage: "url(https://i.ibb.co/LtXVQXj/image.png)",
+            backgroundImage: `url(${image})`,
           }}
         >
           <div className="hero-overlay bg-opacity-50"></div>
           <div className="w-full hero-content text-left text-white">
             <div className="md:w-1/2 ">
               <h1 className="mb-5 text-5xl font-bold">
-                Introducing Our <span>Asset Nex</span> Management System
+                Introducing Our <span>Asset </span> Management System
               </h1>
               <Link to="/join_hr_manager">
                 <Button className="btn btn-primary">Join as HR Manager </Button>
@@ -44,14 +46,14 @@ const BannerSlider = () => {
         <div
           className="hero min-h-screen"
           style={{
-            backgroundImage: "url(https://i.ibb.co/vxpzJh1/image.png)",
+            backgroundImage: `url(${image1})`,
           }}
         >
           <div className="hero-overlay bg-opacity-60"></div>
           <div className="w-full hero-content  text-white">
             <div className="md:w-1/2">
               <h1 className="mb-5 text-5xl font-bold">
-                Introducing Our <span>Asset Nex</span> Management System
+                Introducing Our <span>Asset </span> Management System
               </h1>
               <Link to="/join_employee">
                 <Button className="btn btn-primary">
