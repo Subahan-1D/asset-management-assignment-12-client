7d02e3f9947636107bb584ed1ad8981461b075f3
subahan ali <subahanislam523@gmail.com>
2024-09-11 03:14:12 +0600

    sponer page

--- src/Pages/Home/Sponsor.jsx
+++ src/Pages/Home/Sponsor.jsx
@@ -1,22 +1,32 @@
 import Marquee from "react-fast-marquee";
 
+import img1 from "../../assets/asset.png";
+import img2 from "../../assets/asset-management.png";
+import img3 from "../../assets/hr-manager.png";
+import img4 from "../../assets/asset-allocation.png";
+import img5 from "../../assets/financial-advisor.png";
+import img6 from "../../assets/money-management.png";
+import img7 from "../../assets/project-manager.png";
+import img8 from "../../assets/project.png";
+import img9 from "../../assets/material-management.png";
+import img10 from "../../assets/project-management.png";
+import img11 from "../../assets/management.png";
+import img12 from "../../assets/project-manager.png";
 const Sponsor = () => {
   return (
     <Marquee className="h-48 ">
-      <img
-        src="https://i.ibb.co/XZJqBBV/image.png"
-        alt=""
-        className="h-20 mr-11"
-      />
-      <img
-        src="https://i.ibb.co/zxH7WfM/image.png"
-        alt=""
-        className="h-20 mr-11"
-      />
-      <img src="https://i.ibb.co/16LyT5y/image.png" alt="" className="h-20 mr-11" />
-      <img src="https://i.ibb.co/hdL6HJW/image.png" alt="" className="h-20 mr-11" />
-      <img src="https://i.ibb.co/y4hr2DM/images.jpg" alt="" className="h-20 mr-11" />
-      <img src="https://i.ibb.co/MCpM4FX/image.png" alt="" className="h-20 mr-11" />
+      <img src={img1} alt="" className="h-20 mr-11" />
+      <img src={img2} alt="" className="h-20 mr-11" />
+      <img src={img3} alt="" className="h-20 mr-11" />
+      <img src={img4} alt="" className="h-20 mr-11" />
+      <img src={img5} alt="" className="h-20 mr-11" />
+      <img src={img6} alt="" className="h-20 mr-11" />
+      <img src={img7} alt="" className="h-20 mr-11" />
+      <img src={img8} alt="" className="h-20 mr-11" />
+      <img src={img9} alt="" className="h-20 mr-11" />
+      <img src={img10} alt="" className="h-20 mr-11" />
+      <img src={img11} alt="" className="h-20 mr-11" />
+      <img src={img12} alt="" className="h-20 mr-11" />
     </Marquee>
   );
 };
