798e243fc9aae0a7c686f9274b8ad06fb24825e0
subahan ali <subahanislam523@gmail.com>
2024-09-12 23:07:58 +0600

    new deploy

--- src/Hooks/useAxiosPublic.jsx
+++ src/Hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "http://localhost:9000",
+  baseURL: "https://asset-management-assignment-12-server.vercel.app",
 });
 const useAxiosPublic = () => {
   return axiosPublic;
--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:9000",
+  baseURL: "https://asset-management-assignment-12-server.vercel.app",
 });
 const useAxiosSecure = () => {
   const navigate = useNavigate();
